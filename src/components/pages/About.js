93c3c297c1e36014f2c119d6b5c8695f18dcf185
Jeff Greco <jeffpgreco@gmail.com>
2020-02-26 09:44:46 -0500

    added pre-rendering of portfolio component to avoid load time

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Pic from "../../images/pic.png"
+import Portfolio from "./Portfolio"
 
 class About extends Component {
 
@@ -13,26 +14,33 @@ class About extends Component {
                         <div className="card horizontal z-depth-5" id="horizontal">
                             <div className="card-image">
                                 <img src={Pic} id="profile-pic" alt="pic"></img>
-                                    <span className="card-title" id="pic-text">About Me</span>
+                                <span className="card-title" id="pic-text">About Me</span>
                             </div>
-                                <div className="card-content">
-                                    <p>Full Stack Web Developer with a certificate from the University of
-                                        North Carolina Charlotte. I am skilled in using Javascript/ES6,
-                                        React.js, MongoDB/Mongoose, MySQL/Sequelize, Express.js, Html and CSS.
-                                        I also have experience with various CSS libraries including Bootstrap,
-                                        Materialize and CSS Grid. I am a well rounded developer with a strong
-                                        focus on having an intuitive and efficient user experience. I find my
-                                        approach to parsing and assimilating new knowledge as well as my ability
-                                        to work well with people of all different personalities to be amongst my
-                                        greatest assets. I look forward to growing with a company and getting
+                            <div className="card-content">
+                                <p>Full Stack Web Developer with a certificate from the University of
+                                    North Carolina Charlotte. I am skilled in using Javascript/ES6,
+                                    React.js, MongoDB/Mongoose, MySQL/Sequelize, Express.js, Html and CSS.
+                                    I also have experience with various CSS libraries including Bootstrap,
+                                    Materialize and CSS Grid. I am a well rounded developer with a strong
+                                    focus on having an intuitive and efficient user experience. I find my
+                                    approach to parsing and assimilating new knowledge as well as my ability
+                                    to work well with people of all different personalities to be amongst my
+                                    greatest assets. I look forward to growing with a company and getting
                                 new opportunities to improve my abilities. </p>
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                );
-            }
-        }
-        
+
+                {/* Pre-rendering of Portfolio upon initial page load */}
+                <div id="hidden">
+                    <Portfolio />
+                </div>
+
+            </div>
+
+        );
+    }
+}
+
 export default About;
