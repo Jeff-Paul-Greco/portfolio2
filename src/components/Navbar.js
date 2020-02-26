5d6b7248a2144b0d1429bc34d20b87a32ef330e8
Jeff Greco <jeffpgreco@gmail.com>
2020-02-26 06:01:11 -0500

    added shell of portfolio section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,14 @@
 import React from "react";
-import { Dropdown, Button, Divider, Icon } from 'react-materialize';
+import { Dropdown, Divider } from 'react-materialize';
 
 function Navbar(props) {
 
     return (
 
         <div className="navbar-fixed">
-            {/* <ul id="dropdown1" className="dropdown-content">
-                <li><a href="#!" className="waves-effect waves-purple white-text">About Me</a></li>
-                <li className="divider"></li>
-                <li><a href="#!" className="waves-effect waves-purple white-text">Portfolio</a></li>
-                <li className="divider"></li>
-                <li><a href="#!" className="waves-effect waves-purple white-text">Contact</a></li>
-                <li className="divider"></li>
-                <li><a href="#!"
-                    className="waves-effect waves-purple white-text" target="_blank">Resume</a></li>
-            </ul> */}
             <nav>
                 <div className="nav-wrapper black z-depth-3">
                     <a href="#!" onClick={() => props.handlePageChange("About")} className="brand-logo center" id="nav-logo">Jeff Greco</a>
-                    {/* <ul className="right hide-on-med-and-down">
-                        <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Menu<i
-                            className="material-icons right">arrow_drop_down</i></a></li>
-                    </ul> */}
-
                     <ul className="right hide-on-large-only">
                         <li><a href="#!" onClick={() => props.handlePageChange("Portfolio")}>Portfolio</a></li>
                         <li><a href="#!" onClick={() => props.handlePageChange("Contact")}>Contact</a></li>
@@ -53,7 +38,7 @@ function Navbar(props) {
                             <a href="#!" onClick={() => props.handlePageChange("About")} className="waves-effect waves-purple white-text">
                                 About</a>
                             <Divider />
-                            <a href="#!" onClick={() => props.handlePageChange("Contact")} lassName="waves-effect waves-purple white-text">
+                            <a href="#!" onClick={() => props.handlePageChange("Contact")} className="waves-effect waves-purple white-text">
                                 Contact</a>
                             <Divider />
                             <a href="#!" onClick={() => props.handlePageChange("Portfolio")} className="waves-effect waves-purple white-text">
@@ -64,11 +49,8 @@ function Navbar(props) {
                             <Divider />
                         </Dropdown>
                     </ul>
-
                 </div>
-
             </nav>
-
         </div>
     );
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,14 +1,102 @@
 import React, { Component } from "react";
+import CustomCorner from "../../images/custom-corner.PNG"
+import GoogleBooks from "../../images/google-books.PNG"
+// import Carousel from 'react-responsive-carousel';
 
 class Portfolio extends Component {
-    
+
     render() {
-
         return (
+            // <Carousel showArrows={true} >
+            //     <div>
+            //         <img src="#!" alt="" />
+            //         <p className="legend">Legend 1</p>
+            //     </div>
+            //     <div>
+            //         <img src="#!" alt="" />
+            //         <p className="legend">Legend 2</p>
+            //     </div>
+            //     <div>
+            //         <img src="#!" alt="" />
+            //         <p className="legend">Legend 3</p>
+            //     </div>
+            //     <div>
+            //         <img src="#!" alt="" />
+            //         <p className="legend">Legend 4</p>
+            //     </div>
+            //     <div>
+            //         <img src="assets/5.jpeg" alt="" />
+            //         <p className="legend">Legend 5</p>
+            //     </div>
+            //     <div>
+            //         <img src="assets/6.jpeg" alt="" />
+            //         <p className="legend">Legend 6</p>
+            //     </div>
+            // </Carousel>
+            <div className="carousel carousel-slider">
 
-            <h1>Portfolio</h1>
+                <div className="carousel-fixed-item center-align center" id="carousel-footer">
+
+                    <h3 id="project">My Projects</h3>
+                    <a href="#!" className="btn-large waves-effect waves-purple light-blue black-text darken-text-2" id="prev">prev</a>
+                    <a href="#!" className="btn-large waves-effect waves-purple light-blue black-text darken-text-2" id="next">next</a>
+                </div>
+
+                <div src={CustomCorner} alt="custom-corner" className="carousel-item black white-text pointer" id="custom-corner">
+                <p id="click-project"></p>
+                </div>
+
+                <div src={GoogleBooks} alt="google-books" className="carousel-item black white-text pointer" id="custom-corner">
+                <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="google-books">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="scraper">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="clicky">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="trivia">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="doomsday">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="house-party">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="train">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="friend">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="burger">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="liri">
+                    <p id="click-project"></p>
+                </div>
+
+                <div className="carousel-item black white-text pointer" id="bamazon">
+                    <p id="click-project"></p>
+                </div>
+
+            </div>
         );
     }
-}
+};
 
 export default Portfolio;
