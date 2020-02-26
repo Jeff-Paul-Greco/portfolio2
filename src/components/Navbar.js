9aa760f1b18f7b4f639722b0db607c9ea6292de4
Jeff Greco <jeffpgreco@gmail.com>
2020-02-26 04:03:21 -0500

    fixed dropdown placement

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Dropdown, Button, Divider, Icon } from 'react-materialize';
 
 function Navbar(props) {
 
@@ -18,17 +19,64 @@ function Navbar(props) {
             <nav>
                 <div className="nav-wrapper black z-depth-3">
                     <a href="#!" onClick={() => props.handlePageChange("About")} className="brand-logo center" id="nav-logo">Jeff Greco</a>
-                    <ul className="right hide-on-med-and-down">
+                    {/* <ul className="right hide-on-med-and-down">
                         <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Menu<i
                             className="material-icons right">arrow_drop_down</i></a></li>
-                    </ul>
+                    </ul> */}
+
                     <ul className="right hide-on-large-only">
                         <li><a href="#!" onClick={() => props.handlePageChange("Portfolio")}>Portfolio</a></li>
                         <li><a href="#!" onClick={() => props.handlePageChange("Contact")}>Contact</a></li>
                         <li><a href="https://drive.google.com/open?id=1EGxcgCeX5JwdVODhA8utjJ10ZZQopUyX" target="_blank" rel="noopener noreferrer">Resume</a></li>
                     </ul>
+                    <ul className="right hide-on-med-and-down">
+                        <Dropdown className="right"
+                            options={{
+                                alignment: 'right',
+                                autoTrigger: true,
+                                closeOnClick: true,
+                                constrainWidth: true,
+                                container: null,
+                                coverTrigger: true,
+                                hover: true,
+                                inDuration: 150,
+                                onCloseEnd: null,
+                                onCloseStart: null,
+                                onOpenEnd: null,
+                                onOpenStart: null,
+                                outDuration: 250
+                            }}
+                            trigger={<Button node="button" id="dropdown">Menu</Button>}
+                        >
+                            <a href="#">
+                                one
+  </a>
+                            <a href="#">
+                                two
+  </a>
+                            <Divider />
+                            <a href="#">
+                                three
+  </a>
+                            <a href="#">
+                                <Icon>
+                                    view_module
+    </Icon>
+                                four
+  </a>
+                            <a href="#">
+                                <Icon>
+                                    cloud
+    </Icon>
+                                {' '}five
+  </a>
+                        </Dropdown>
+                    </ul>
+
                 </div>
+
             </nav>
+
         </div>
     );
 
