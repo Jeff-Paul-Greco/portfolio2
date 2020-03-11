a3c29c78fed1348fe96c35dcd4f0e2538cb13609
Jeff Greco <jeffpgreco@gmail.com>
2020-03-11 06:59:51 -0400

    testing heroku deploy

---
+++ client/build/precache-manifest.1d7a69181a4c0ad0e960cd96cec08623.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb9fa3467a8957c124de9c62b9e16f03",
+    "url": "/static/media/train.bb9fa346.png"
+  },
+  {
+    "revision": "891a81594e29059e912f",
+    "url": "/static/css/main.5fd32f97.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9c162f59aa5c085d3acdc1e5f072df40",
+    "url": "/static/media/custom-corner.9c162f59.PNG"
+  },
+  {
+    "revision": "9d31dd8359f5dc5bb529",
+    "url": "/static/js/2.28200754.chunk.js"
+  },
+  {
+    "revision": "5ba25b379679d0658ea9aad803ddb2a9",
+    "url": "/static/media/bamazon.5ba25b37.png"
+  },
+  {
+    "revision": "5282759b05dd9187bb9b6771ae0ab98e",
+    "url": "/static/media/liri.5282759b.png"
+  },
+  {
+    "revision": "c78a6127bde93fc7b53b4cb836ef1b86",
+    "url": "/static/media/doomsday.c78a6127.png"
+  },
+  {
+    "revision": "0d7a103ae636f7c53b0f04a74db14431",
+    "url": "/static/media/background2.0d7a103a.png"
+  },
+  {
+    "revision": "4b7efd2f0aa55c646bc88ad4c51770dc",
+    "url": "/static/media/burger.4b7efd2f.png"
+  },
+  {
+    "revision": "54457505ec79bf713881b88738bffa8b",
+    "url": "/static/media/gif.54457505.png"
+  },
+  {
+    "revision": "96101feca8d806644b5430dd5eb3c8c2",
+    "url": "/static/media/psychic.96101fec.png"
+  },
+  {
+    "revision": "891a81594e29059e912f",
+    "url": "/static/js/main.88c38973.chunk.js"
+  },
+  {
+    "revision": "f369cc07211014d75be4762381341e97",
+    "url": "/static/media/navbar.f369cc07.jpg"
+  },
+  {
+    "revision": "ae45e4ace38189f8123e2ba74a729693",
+    "url": "/static/media/house-party.ae45e4ac.png"
+  },
+  {
+    "revision": "a5f253365756dd74835545161546e033",
+    "url": "/static/media/background.a5f25336.jpg"
+  },
+  {
+    "revision": "d1e1b01add4ebd581ec5b56d99dcc56e",
+    "url": "/static/media/trivia.d1e1b01a.png"
+  },
+  {
+    "revision": "56f894ee0f98a774a55d2ac12a70ef2f",
+    "url": "/static/media/friend-finder.56f894ee.png"
+  },
+  {
+    "revision": "4b56a347f1e8c9f0af46faf235fdd7d1",
+    "url": "/static/media/google-books.4b56a347.PNG"
+  },
+  {
+    "revision": "27e10960df3e9dd7e1f05b431605e5ac",
+    "url": "/static/media/scraper.27e10960.PNG"
+  },
+  {
+    "revision": "9edb0fd2b44a4e533aca2ee5c5824573",
+    "url": "/static/media/clicky.9edb0fd2.PNG"
+  },
+  {
+    "revision": "957cd7cf4d2498bdcd0c05c2c00b992c",
+    "url": "/static/media/pic.957cd7cf.png"
+  },
+  {
+    "revision": "da6dc03f1777f8453b160e22c7f115b1",
+    "url": "/static/media/email.da6dc03f.png"
+  },
+  {
+    "revision": "9d31dd8359f5dc5bb529",
+    "url": "/static/css/2.565ed421.chunk.css"
+  },
+  {
+    "revision": "98e598e2b70f4664f06b3f0a64e5fd95",
+    "url": "/index.html"
+  }
+];
